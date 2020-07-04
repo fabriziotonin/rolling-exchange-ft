dda061ed793d857b3ee05994e3e83d19b6d788e1
Your Name <xfitex>
2020-07-04 15:15:23 -0300

    add new pallet colors

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './src/constants/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Bienvenido a Rolling exchange</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.background,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "#fffff",
+  }
 });
---
+++ src/constants/colors.js
@@ -0,0 +1,7 @@
+export default colors = {
+  background: '#0B132B',
+  primary: '#1C2541',
+  secondary: '#6FFFE9',
+  links: '#5BC0BE',
+  text: '#FFFFFF'
+}
