824f62e1e4f5e7c3f54bb279d780cf3ed90853c1
Your Name <xfitex>
2020-07-10 15:38:38 -0300

    new colors add

---
+++ src/components/TopContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet} from 'react-native';
+
+// LightTheme or DarkTheme 
+import { darkTheme as theme } from '../constants/colors';
+
+export default function TopContainer() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,7 +1,24 @@
-export default colors = {
-  background: '#0B132B',
-  primary: '#1C2541',
-  secondary: '#6FFFE9',
-  links: '#5BC0BE',
-  text: '#FFFFFF'
+export const darkTheme = {
+  primary: '#171921',
+  secondary: '#F7F7F7',
+  container: '#242D36',
+  textPrimary: '#FFFFFF',
+  textSecondary: '#000000',
+  link: '#F85D5D',
 }
+
+export const lightTheme = {
+  primary: '#ebebeb',
+  secondary: '#171921',
+  container: '#FFFFFF',
+  textPrimary: '#171921',
+  textSecondary: '#FFFFFF',
+  link: '#FC8C03',
+}
+
+// Mis colores
+// background: '#0B132B',
+// primary: '#1C2541',
+// secondary: '#6FFFE9',
+// links: '#5BC0BE',
+// text: '#FFFFFF'
