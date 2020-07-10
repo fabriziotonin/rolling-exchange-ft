22d642ccda9f18167cbc41580095ecde06d84a92
Your Name <xfitex>
2020-07-10 15:31:14 -0300

    add assets flag and constant flag.js

--- App.js
+++ App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, Fragment } from 'react';
+import { StyleSheet, View, StatusBar, Platform } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 import colors from './src/constants/colors';
 
+
+// LightTheme or darkTheme
+import { LightTheme as theme } from './src/constants/colors'
+
 export default function App() {
+  const [mainVisible, setMainVisible] = useState(true);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Bienvenido a Rolling exchange</Text>
-    </View>
+    <PaperProvider>
+      {Platform.Os === 'ios' && <StatusBar barStyle="default" />}
+      {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+
+      {
+        mainVisible ?
+        (
+          <Fragment>
+
+          </Fragment>
+        )
+        :
+        (
+          <Fragment>
+
+          </Fragment>
+        )
+      }
+      
+    </PaperProvider>
   );
 }
 
+
+
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.background,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: colors.text,
-  }
+statusBarUnderlay: {
+  height: 28,
+  backgroundColor: theme.secondary,
+}
 });
 
---
+++ src/constants/flags.js
@@ -0,0 +1,7 @@
+const flags = {
+  arg: require('../assets/flags/arg.png'),
+  eur: require('../assets/flags/eur.png'),
+  jpy: require('../assets/flags/jpy.png'),
+  usd: require('../assets/flags/usd.png')
+}
+export default flags;
